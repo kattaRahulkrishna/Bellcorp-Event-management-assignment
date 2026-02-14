25928517d5d175890e849e15aa1aa259c67cb59b
kattaRahulkrishna <kattarahulkrishna05853@gmail.com>
2026-02-14 13:28:07 +0530

    Fix variable name in api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const instance = axios.create({
+const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
         ? `${import.meta.env.VITE_API_URL}/api`
         : '/api',
